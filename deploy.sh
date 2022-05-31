9b45a67ce161f8aa88149c8546f61fd4a19534de
coulsonzero <coulson2021@163.com>
2022-05-31 18:29:32 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 yarn build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd build
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy react-dashboard'
 
 # 如果发布到https://<USERNAME>.github.io
-# git push -f git@github.com:coulsonzero/docs.git
+# git push -f git@github.com:coulsonzero/react-dashboard.git
 
 
 # 如果发布到https://<USERNAME>.github.io/<REPO>
