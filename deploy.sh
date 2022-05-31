28e3a21cca02399d02991d17d717661d34fdac83
coulsonzero <coulson2021@163.com>
2022-05-31 18:23:25 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+# echo 'docs.coulsonzero.top' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy react-dashboard'
+
+# 如果发布到https://<USERNAME>.github.io
+# git push -f git@github.com:coulsonzero/docs.git
+
+
+# 如果发布到https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:coulsonzero/react-dashboard.git master:gh-pages
+
+cd -
+
+rm -rf build
